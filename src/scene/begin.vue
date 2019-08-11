9cd5f80723955b73eedf1cbfaa2e3b155bf854f2
xm94630 <xm94630@126.com>
2019-08-11 21:38:52 +0800

    添加router;添加webpack-bundle-analyzer

--- src/scene/begin.vue
+++ src/scene/begin.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="bg10">
     游戏开场
+    <div class="xxx">测试</div>
   </div>
 </template>
 
@@ -14,5 +15,10 @@ export default {
 </script>
 
 
-<style scoped>
+<style scoped lang="scss">
+.bg10{
+  .xxx{
+    color:red;
+  }
+}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,8 @@
 module.exports={
     publicPath: './',
-    outputDir: 'datas_build',  
+    outputDir: 'dist',  
     assetsDir: 'static',
     runtimeCompiler:true,
-    // configureWebpack: {
-    //     devServer: {
-    //         proxy: {
-    //             '/epimetheus': {
-    //                 target: 'http://localhost:3000',
-    //                 secure: false, 
-    //             },
-    //         }
-    //     },
-    //     resolve: {
-    //         alias: {
-    //           '@': __dirname
-    //         }
-    //     },
-    // },
-
     // chainWebpack: config => {
     //     config
     //       .plugin('webpack-bundle-analyzer')
